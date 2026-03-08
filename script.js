c8195d31e81eded94f7274c1b8251155255c7754
affan2205 <fandelove2205@gmail.com>
2026-03-08 23:31:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+/* LOGIN MESSAGE */
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
 
+    alert("Login berhasil! Selamat datang di Poda Na Lima Library");
+});
+
+
+/* NAVBAR EFFECT SAAT SCROLL */
+window.addEventListener("scroll", function(){
+
+    let navbar = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+        navbar.style.background = "rgba(0,0,0,0.9)";
+    } else {
+        navbar.style.background = "rgba(0,0,0,0.6)";
+    }
+
+});
+
+
+/* ANIMASI SAAT SCROLL */
+let sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", function(){
+
+    let trigger = window.innerHeight * 0.8;
+
+    sections.forEach(section => {
+
+        let top = section.getBoundingClientRect().top;
+
+        if(top < trigger){
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+        }
+
+    });
+
+});
